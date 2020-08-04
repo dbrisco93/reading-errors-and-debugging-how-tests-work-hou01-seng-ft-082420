7a0b58c75e269d7a0e8868a820a973bd95ae1615
Dominique Brisco <dbrisco93@gmail.com>
2020-08-04 05:10:24 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,2 @@
 # Add your variables here
-do
-  first_number = get_variable_from_file('./calculator.rb', "first_number")
-  
-  expect(first_number).to be_an(Integer).or be_a(Float)
-end
+first_number = "12"
